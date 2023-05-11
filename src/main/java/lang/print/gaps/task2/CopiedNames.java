867c1cc909c8de35385ff14607498883bd89612a
m4loo <128167781+m4loo@users.noreply.github.com>
2023-05-12 04:43:49 +0700

    done

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,16 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        variable = 2000;
-        int value = 1000;
-        variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
-        value = 90000;
+        int variable2 = 2000;
+        int variable3 = 1000;
+        int variable4 = 4000;
+        int variable5 = 3000;
+        int variable6 = 5000;
+        variable6 = 7000;
+        int variable7 = 90000;
+        int variable8 = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myInteger2 = 1234567;
     }
 }
